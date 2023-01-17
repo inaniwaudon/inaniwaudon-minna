f0702d1e0cdcb0abfc60322965dabb3b4bad43d2
inaniwaudon <w.yuto0916@gmail.com>
2023-01-17 22:39:32 +0900

    fix bugs

--- src/components/common/Page.tsx
+++ src/components/common/Page.tsx
@@ -5,6 +5,10 @@ import Footer from '@/components/common/Footer';
 
 const Wrapper = styled.div`
   margin: 30px 50px;
+
+  @media screen and (max-width: 500px) {
+    margin: 30px 30px;
+  }
 `;
 
 interface PageProps {
