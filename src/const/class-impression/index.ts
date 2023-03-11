ee3f6518839d75415fb27aa7cab21c39df09335f
inaniwaudon <w.yuto0916@gmail.com>
2023-03-11 13:12:02 +0900

    add class-impression

---
+++ src/const/class-impression/index.ts
@@ -0,0 +1,36 @@
+import markdown2021spring from './2021spring.md';
+import markdown2021autumn from './2021autumn.md';
+import markdown2022spring from './2022spring.md';
+import markdown2022autumn from './2022autumn.md';
+
+export const classImpressions: {
+  year: number;
+  term: 'spring' | 'autumn';
+  description: string;
+  article: string;
+}[] = [
+  {
+    year: 2021,
+    term: 'spring',
+    description: 'ワクワクドキドキ初めての筑波大学',
+    article: markdown2021spring,
+  },
+  {
+    year: 2021,
+    term: 'autumn',
+    description: '筑波の秋は綺麗です',
+    article: markdown2021autumn,
+  },
+  {
+    year: 2022,
+    term: 'spring',
+    description: 'いやーマジで苦しかった春ABモジュールが終わりましたねー',
+    article: markdown2022spring,
+  },
+  {
+    year: 2022,
+    term: 'autumn',
+    description: '飽きがきた秋学期',
+    article: markdown2022autumn,
+  },
+];
