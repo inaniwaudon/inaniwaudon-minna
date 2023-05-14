e83fb43071a21b0d4a8f09aa68d664f808f0d038
inaniwaudon <w.yuto0916@gmail.com>
2023-05-14 23:06:55 +0900

    fix details

--- src/pages/articles/max/index.tsx
+++ src/pages/articles/max/index.tsx
@@ -6,12 +6,13 @@ import g2sanserif from '@/assets/max/g2sanserif.webp';
 import logo from '@/assets/max/logo.svg';
 import maxLogo from '@/assets/max/max-logo.webp';
 import maxLogoAnalyze from '@/assets/max/max-logo-analyze.webp';
-import maxMakuhariImage from '@/assets/max/max-makuhari.jpeg';
+import maxMakuhariImage from '@/assets/max/max-makuhari.webp';
 import maxRedImage from '@/assets/max/max-red.webp';
 import maxTrace from '@/assets/max/max-trace.webp';
 import scannerImage from '@/assets/max/scanner.webp';
 import scannedImage from '@/assets/max/scanned.webp';
 import tel from '@/assets/max/tel.webp';
+import thumbnail from '@/assets/max/thumbnail.webp';
 import wave from '@/assets/max/wave.webp';
 import Footer from '@/components/common/Footer';
 
@@ -27,6 +28,18 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
+const ThumbnailImage = styled.div`
+  width: 100%;
+  height: 440px;
+  background: url(${thumbnail.src});
+  background-position: center;
+  background-size: cover;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -1;
+`;
+
 const Page = styled.div`
   width: 800px;
   margin: 32px auto;
@@ -40,6 +53,7 @@ const Page = styled.div`
 const Wrapper = styled.div`
   display: flex;
   gap: 40px;
+  position: relative;
 
   @media screen and (max-width: ${800 + 32 * 2}px) {
     justify-content: center;
@@ -48,6 +62,8 @@ const Wrapper = styled.div`
 
 const SideNavigation = styled.nav`
   width: 160px;
+  position: relative;
+  z-index: 2;
 
   @media screen and (max-width: 800px) {
     display: none;
@@ -62,7 +78,7 @@ const SideNavigation = styled.nav`
   }
 
   li {
-    color: ${maxBrown};
+    color: #fff;
     padding: 6px 8px 8px 8px;
 
     &:hover {
@@ -72,7 +88,7 @@ const SideNavigation = styled.nav`
   }
 
   li + li {
-    border-top: solid 1px ${maxBrown};
+    border-top: solid 1px #fff;
   }
 
   a {
@@ -83,10 +99,23 @@ const SideNavigation = styled.nav`
 
 const Content = styled.main`
   width: 600px;
+  position: relative;
+  z-index: 1;
 `;
 
 const H1 = styled.h1`
+  max-width: 300px;
   margin: 0;
+`;
+
+const Abstract = styled.section`
+  height: 290px;
+  color: #fff;
+  text-shadow: 0 1px 4px rgba(0, 0, 0, 0.6);
+`;
+
+const AbstractContent = styled.div`
+  max-width: 300px;
 `;
 
 const H2 = styled.h2`
@@ -189,6 +218,7 @@ const Index = () => {
         <title>マックスコーヒーのパッケージ観察｜いなにわうどん.みんな</title>
       </Head>
       <GlobalStyle />
+      <ThumbnailImage />
       <Page>
         <Wrapper>
           <SideNavigation>
@@ -207,15 +237,14 @@ const Index = () => {
               <H1>
                 <img src={logo.src} alt="マックスコーヒーのパッケージ観察" />
               </H1>
-              <Paragraph>マックスコーヒー飲んでますか！？</Paragraph>
-              <Paragraph>
-                コカ・コーラから北関東を中心に発売されているマックスコーヒー（図
-                1）。甘みの強い練乳入りのコーヒーと、黄色と茶色で構成された可愛らしいパッケージが特徴的です。本記事では、そんな特色あるマックスコーヒーのデザインに着目し、マッ缶ことマックスコーヒー缶のパッケージの装飾を調べました。
-              </Paragraph>
-              <Figure>
-                <FigureImage src={maxMakuhariImage.src} alt="" />
-                <Caption>図 1：コンビニに並ぶマックスコーヒー</Caption>
-              </Figure>
+              <Abstract>
+                <AbstractContent>
+                  <Paragraph>マックスコーヒー飲んでますか！？</Paragraph>
+                  <Paragraph>
+                    コカ・コーラから北関東を中心に発売されているマックスコーヒー。甘みの強い練乳入りのコーヒーと、黄色と茶色で構成された可愛らしいパッケージが特徴的です。本記事では、そんな特色あるマックスコーヒーのデザインに着目し、マッ缶ことマックスコーヒー缶のパッケージの装飾を調べました。
+                  </Paragraph>
+                </AbstractContent>
+              </Abstract>
               <section id="data">
                 <H2>データ集め</H2>
                 <Paragraph>
@@ -226,8 +255,8 @@ const Index = () => {
                 <List>
                   <li>
                     マックスコーヒーの 500ml ペットボトルのフィルムを剥がし 600 dpi
-                    でスキャンしたデータ（図 2、図
-                    3）：フィルムに若干の歪みがあり怪しい点も存在するが、少なくともインターネット上のデータよりは緻密なデータが得られそう。
+                    でスキャンしたデータ（図 1、図
+                    2）：フィルムに若干の歪みがあり怪しい点も存在するが、少なくともインターネット上のデータよりは緻密なデータが得られそう。
                   </li>
                   <li>
                     マッ缶を望遠レンズで撮影したデータ：遠距離から望遠で撮影することで幾分か歪みが緩和される。
@@ -235,42 +264,43 @@ const Index = () => {
                 </List>
                 <Figure>
                   <FigureImage src={scannerImage.src} alt="" />
-                  <Caption>図 2：スキャンの光景</Caption>
+                  <Caption>図 1：スキャンの光景</Caption>
                 </Figure>
                 <Figure>
                   <FigureImage src={scannedImage.src} alt="" />
-                  <Caption>図 3：スキャンしたデータ</Caption>
+                  <Caption>図 2：スキャンしたデータ</Caption>
                 </Figure>
                 <Paragraph>
                   缶のサイズを計測したところ高さ 125 mm、Φ51 mm、円周が 168 mm
-                  でした。これらのデータを基にトレースしたデータを図 4 に示します。
+                  でした。これらのデータを基にトレースしたデータを図 3 に示します。
                 </Paragraph>
                 <Figure>
                   <FigureImage src={maxTrace.src} alt="" border={true} />
-                  <Caption>図 4：トレースした缶のパッケージ</Caption>
+                  <Caption>図 3：トレースした缶のパッケージ</Caption>
                 </Figure>
               </section>
               <section id="logo">
                 <H2>ロゴ</H2>
                 <Paragraph>
                   正面と背面の 2 箇所に「MAX COFFEE」のロゴ（図
-                  5）が印刷されています。正面のロゴのサイズを計測したところ、50 mm × 26.7 mm
-                  でした。書体の分類としてはサンセリフ体に該当し、Gotham
-                  等に通ずる幾何学的なデザインです。
+                  4）が印刷されています。恐らくオリジナルのロゴと推測されますが、書体の分類としてはサンセリフ体に該当し、Gotham
+                  等に通ずる幾何学的なデザインです。正面のロゴのサイズを計測したところ、50 mm × 26.7
+                  mm でした。
                 </Paragraph>
                 <Paragraph>
-                  全体に目を向けると、「MAX」の天地（上下）が完全に揃っているのに対し、「COFFEE」の部分は
-                  C、O
-                  は上下のラインから僅かに飛び出すような形で配置されています。水平方向では、「COFFEE」が「MAX」に比べて左（缶では上向き）に寄っていることも読み取れます。
+                  全体に目を向けると、「MAX」の天地（上下）が完全に揃っているのに対し、「COFFEE」の部分では
+                  C、O が上下のラインから僅かに飛び出すような形で配置されています。水平方向では、C
+                  のカーブが M
+                  の左端よりも出っ張っていますが、右側はおおよそ垂直で揃えられています。
                 </Paragraph>
                 <Figure>
                   <FigureImage src={maxLogo.src} alt="MAX COFFEE" />
-                  <Caption>図 5：「MAX COFFEE」のロゴ</Caption>
+                  <Caption>図 4：「MAX COFFEE」のロゴ</Caption>
                 </Figure>
                 <h3>In depth</h3>
                 <Paragraph>
                   MAX の文字をより詳しく見てみます（図
-                  6）。一般に、書体は視差調節を重視して設計されていますが、本ロゴにもそのような調整が加えられているのかが気になります。
+                  5）。一般に、書体は視差調節を重視して設計されることが多いため、本ロゴにもそのような調整が加えられているのかが気になるところです。
                 </Paragraph>
                 <List>
                   <li>
@@ -305,7 +335,7 @@ const Index = () => {
                 </List>
                 <Figure>
                   <FigureImage src={maxLogoAnalyze.src} alt="MAX COFFEE" />
-                  <Caption>図 6：ロゴの詳細。ピンク色の線は平行</Caption>
+                  <Caption>図 5：ロゴの詳細。ピンク色の線は平行</Caption>
                 </Figure>
                 <Paragraph>
                   なおインターネット上の画像と手元の実物を比較すると、現在発売されている缶・ペットボトル双方のパッケージにおいて、旧来よりも
@@ -316,7 +346,7 @@ const Index = () => {
               <section id="wave">
                 <H2>謎の曲線？ 波線</H2>
                 <Paragraph>
-                  ロゴ横に長さ 95.5 mm の波線（図 7）が 4 箇所あしらわれ、1 本の波線に対して周期が 3
+                  ロゴ横に長さ 95.5 mm の波線（図 6）が 4 箇所あしらわれ、1 本の波線に対して周期が 3
                   回（背面は 3
                   回、縮尺も異なる）ずつ存在します。サイン波のような風貌ですがサイン波ではなく、線幅も一定ではありません。1
                   箇所につき、外側、中央、内側と 3
@@ -325,15 +355,15 @@ const Index = () => {
                 </Paragraph>
                 <Figure>
                   <FigureImage src={wave.src} alt="" />
-                  <Caption>図 7：3 本の異なる破線</Caption>
+                  <Caption>図 6：3 本の異なる破線</Caption>
                 </Figure>
               </section>
               <section id="front">
                 <H2>正面</H2>
                 <Paragraph>
                   正面には「MAX
-                  COFFEE」「GEOGIA」ロゴのほかに「練乳入り」「マックスコーヒー」「コーヒー飲料」の文字
-                  3 つが記載され、左右に 38 mm
+                  COFFEE」「GEOGIA」ロゴのほかに「練乳入り」「マックスコーヒー」「コーヒー飲料」と 3
+                  つの文字が記載され、左右に 38 mm
                   の間隔で先述した波線が並びます。パッケージ全体で使用書体はモリサワの「
                   <strong>
                     <Anchor href="https://www.morisawa.co.jp/fonts/specimen/1473">新ゴ</Anchor>
@@ -346,7 +376,7 @@ const Index = () => {
                 </Paragraph>
                 <MaxCoffeeH3>マックスコーヒー</MaxCoffeeH3>
                 <Paragraph>
-                  「マックスコーヒー」の文字（図 8）は31.5 mm × 5 mm
+                  「マックスコーヒー」の文字（図 7）は 31.5 mm × 5 mm
                   の赤い矩形で囲われており、下部から 21.7 mm の位置に配置されています。書体は新ゴ B
                   の 16 Q で、適切にカーニングが詰められているほか、「コ<strong>ー</strong>ヒ
                   <strong>ー</strong>
@@ -354,23 +384,26 @@ const Index = () => {
                 </Paragraph>
                 <Figure>
                   <FigureImage src={maxRedImage.src} alt="MAX COFFEE" />
-                  <Caption>図 8：「マックスコーヒー」の表記</Caption>
+                  <Caption>図 7：「マックスコーヒー」の表記</Caption>
                 </Figure>
               </section>
               <section id="side">
                 <H2>側面</H2>
                 <Paragraph>
                   側面には、注意表記、バーコード（JAN
-                  コード）、「リサイクルしてね」のロゴ等が縦方向に並んでおり、狭いスペースながらに大量の情報が詰め込まれています。注意書きは最終行まで両端揃えの所謂「カンパコ組み」で、約物は行中・行末に関わらず半角です。全体的に平体気味ですが、項目ごとに本文よりも小さなサイズで付された
-                  ●
-                  のマーカーは正体のように見えます。注意事項の背後のザブトンは角が若干丸まっています。
+                  コード）、「リサイクルしてね」のロゴ等が縦方向に並んでおり、狭いスペースながらに大量の情報が詰め込まれています。注意書きは最終行まで両端揃えの所謂「カンパコ組み」で、約物は行中・行末に関わらず半角です。全体的に長体気味ですが、項目ごとに本文よりも小さなサイズで付された
+                  ● のマーカーは正体に見えます。注意事項の背後のザブトンは角が若干丸まっています。
                 </Paragraph>
                 <Paragraph>
-                  「リサイクルしてね」（図 9） は タイプバンク（旧リョービ）の{' '}
+                  「リサイクルしてね」（図 8） は タイプバンク（旧リョービ）の{' '}
                   <Anchor href="https://www.typebank.co.jp/fontfamily/g2sansserif/">
                     G2 サンセリフ-B
                   </Anchor>{' '}
-                  をベースとしたロゴタイプで、コカ・コーラ社で共通のシンボルが使用されています。「リサイクル」の部分は微調整が加えられ、元のフォントと若干エレメントが異なるほか、全体的に少し太めに見えます。その他の部分に関しては元のフォントと同一の形状です。
+                  をベースとしたロゴタイプで、コカ・コーラ社で共通のシンボルが使用されています。「リサイクル」の部分は微調整が加えられ、元のフォントと若干エレメントが異なるほか、全体的に少し太めに強調されて見えます
+                  <a href="#footnote1">
+                    <sup>[1]</sup>
+                  </a>
+                  。その他の部分に関しては元のフォントと同一の形状です。
                   また、「あき缶はリサイクル」の部分は <strong>UD 新ゴ</strong>
                   が採用されています。パッケージの他箇所が通常の新ゴであることに鑑みると、この部分も他飲料と共通である可能性が高いです。
                 </Paragraph>
@@ -379,14 +412,15 @@ const Index = () => {
                     src={g2sanserif.src}
                     alt="G2 サンセリフ リサイクルしてね リサイクルしてね"
                   />
-                  <Caption>図 9：「リサイクルしてね」のロゴタイプとG2サンセリフ</Caption>
+                  <Caption>図 8：「リサイクルしてね」のロゴタイプとG2サンセリフ</Caption>
                 </Figure>
               </section>
               <section id="components">
                 <H2>成分表示</H2>
                 <Paragraph>
                   複数の書体サイズが混在しています（図
-                  10）。「0120-308509」の電話番号は隣の「お客様相談室」よりも級上げされ、書体も太めのウェイトが採用されています。市外局番を区切るハイフンもベースラインシフトによって天地中央に揃えられるなど、細部まで拘った調整が伺えます。その下の
+                  9）。「0120-308509」の電話番号は隣の「お客様相談室」よりも級上げされ、書体も太めのウェイトが採用されています（図
+                  10）。市外局番を区切るハイフンもベースラインシフトによって天地中央に揃えられるなど、細部まで拘った調整が伺えます。その下の
                   「GEOGIA」のみ Helvetica が使用されています。
                 </Paragraph>
                 <Figure>
@@ -394,7 +428,7 @@ const Index = () => {
                     src={compositions.src}
                     alt="G2 サンセリフ リサイクルしてね リサイクルしてね"
                   />
-                  <Caption>図 10：裏面の成分表示欄</Caption>
+                  <Caption>図 9：裏面の成分表示欄</Caption>
                 </Figure>
                 <Figure>
                   <FigureImage src={tel.src} alt="お客様相談室 0120-308509" />
@@ -409,7 +443,10 @@ const Index = () => {
                 <Paragraph>
                   以下の 4
                   色が使用されています。文字周囲に僅かなヌケ（白い隙間）が見られることから、オーバープリント処理
-                  <sup>[1]</sup>はされていないようです。特色を使用している可能性もあります。
+                  <Anchor href="#footnote1">
+                    <sup>[2]</sup>
+                  </Anchor>
+                  はされていないようです。特色が使用されている可能性もあります。
                 </Paragraph>
                 <Table>
                   <tbody>
@@ -452,13 +489,26 @@ const Index = () => {
               <Paragraph>
                 いかがでしたか!?
                 調べてみると、マックスコーヒーのパッケージには様々な工夫が凝らされており、北関東のソウルフードとして愛されてきた所以が判明しました！
-                今後もマックスコーヒーから目が離せません！！！
+                今後もマックスコーヒー（図 11）から目が離せません！！！
               </Paragraph>
+              <Figure>
+                <FigureImage src={maxMakuhariImage.src} alt="" />
+                <Caption>図 11：コンビニに陳列されたマックスコーヒー</Caption>
+              </Figure>
               <Paragraph>
                 本記事は独自研究に基づくものであり、コカ・コーラ社および株式会社鈴木コーヒーとは一切関係ありません。
               </Paragraph>
             </main>
-            <aside>1: オーバープリント処理の解説</aside>
+            <aside>
+              <p id="footnote1">
+                [1] G2 サンセリフ-U では太すぎるため、あくまで G2 サンセリフ-B
+                をベースに加工したロゴのようです。
+              </p>
+              <p id="footnote2">
+                [2]
+                オーバープリント処理：版ズレが起こった際に白い箇所が目立たないように、色を重ねて印刷すること。
+              </p>
+            </aside>
           </Content>
         </Wrapper>
         <Footer />
