7b4376f7a9f050b4bba73ddeb6845dc690325087
inaniwaudon <w.yuto0916@gmail.com>
2023-05-30 15:02:30 +0900

    implement tags function

---
+++ src/components/common/Checkbox.tsx
@@ -0,0 +1,85 @@
+import styled from 'styled-components';
+
+const CategoryList = styled.ul`
+  margin: 0;
+  padding: 4px 0 0 0;
+  list-style: none;
+  display: flex;
+  gap: 10px;
+`;
+
+const CategoryItemCheck = styled.div<{ selected: boolean; keyColor: string }>`
+  width: 8px;
+  height: 8px;
+  border-radius: 50%;
+  background: ${({ selected, keyColor }) => (selected ? '#fff' : keyColor)};
+  opacity: ${({ selected }) => (selected ? 1 : 0.2)};
+  transition: opacity 0.2s;
+`;
+
+const Button = styled.button<{ selected: boolean; keyColor: string }>`
+  height: 14px;
+  line-height: 14px;
+  color: ${({ selected, keyColor }) => (selected ? '#fff' : keyColor)};
+  font-size: 14px;
+  padding: 6px 8px 8px 10px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  font-family: inherit;
+  box-shadow: 0 1px 4px rgba(0, 0, 0, 0.1);
+  background: ${({ selected, keyColor }) => (selected ? keyColor : '#fff')};
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  box-sizing: content-box;
+  transition: color 0.2s, background 0.2s;
+
+  &:hover ${CategoryItemCheck} {
+    opacity: 1;
+  }
+`;
+
+interface Option {
+  key: string;
+  label: string;
+  keyColor: string;
+}
+
+interface CheckboxProps {
+  options: Option[];
+  multiple: boolean;
+  selectedOptions: string[];
+  setSelectedOptions: (value: string[]) => void;
+}
+
+const Checkbox = ({ options, selectedOptions, setSelectedOptions }: CheckboxProps) => {
+  const onClickCategoryItem = (key: string) => {
+    setSelectedOptions(
+      selectedOptions.includes(key)
+        ? selectedOptions.filter((option) => option !== key)
+        : [...selectedOptions, key]
+    );
+  };
+
+  return (
+    <CategoryList>
+      {options.map((option) => {
+        const selected = selectedOptions.includes(option.key);
+        return (
+          <li key={option.key}>
+            <Button
+              selected={selected}
+              keyColor={option.keyColor}
+              onClick={() => onClickCategoryItem(option.key)}
+            >
+              <CategoryItemCheck selected={selected} keyColor={option.keyColor} />
+              {option.label}
+            </Button>
+          </li>
+        );
+      })}
+    </CategoryList>
+  );
+};
+export default Checkbox;
--- src/pages/articles/index.tsx
+++ src/pages/articles/index.tsx
@@ -1,5 +1,6 @@
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 import styled from 'styled-components';
+import Checkbox from '@/components/common/Checkbox';
 import CustomList from '@/components/common/CustomList';
 import Page from '@/components/common/Page';
 import PageAnchor from '@/components/common/PageAnchor';
@@ -12,10 +13,14 @@ import {
 } from '@/const/articles';
 
 const H1 = styled.h1`
-  margin: 0 0 10px 0;
+  margin: 0 0 8px 0;
 `;
 
-const Header = styled.header`
+const TopHeader = styled.header`
+  margin-bottom: 16px;
+`;
+
+const ArticleHeader = styled.header`
   font-size: 14px;
 `;
 
@@ -35,6 +40,14 @@ const Description = styled.span`
 `;
 
 const Index = () => {
+  const tags = [
+    { key: 'hongoshi', label: 'hongoshi', keyColor: '#ff32ab' },
+    { key: 'tech', label: 'tech', keyColor: '#cc22db' },
+    { key: 'design', label: 'design', keyColor: '#2656f3' },
+    { key: 'random', label: 'random', keyColor: '#009ae1' },
+  ];
+  const [selectedTags, setSelectedTags] = useState<string[]>([]);
+
   const links = useMemo(() => {
     const hatena = articleHatenaLinks.map((link) => ({ ...link, description: 'はてなブログ' }));
     const note = articleNoteLinks.map((link) => ({ ...link, description: 'note' }));
@@ -48,18 +61,31 @@ const Index = () => {
     );
   }, [articleHatenaLinks, articleNoteLinks, articleZennLinks, articleWordLinks]);
 
+  const filteredLinks =
+    selectedTags.length > 0
+      ? links.filter((link) => link.tags && selectedTags.some((tag) => link.tags!.includes(tag)))
+      : links;
+
   const title = '書いたもの・こと';
 
   return (
     <Page title={title}>
       <main>
-        <H1>{title}</H1>
+        <TopHeader>
+          <H1>{title}</H1>
+          <Checkbox
+            options={tags}
+            multiple={true}
+            selectedOptions={selectedTags}
+            setSelectedOptions={setSelectedTags}
+          />
+        </TopHeader>
         <CustomList>
-          {links.map((link) => (
+          {filteredLinks.map((link) => (
             <li key={link.href}>
-              <Header>
+              <ArticleHeader>
                 <Time>{link.date}</Time> - <Description>{link.description}</Description>
-              </Header>
+              </ArticleHeader>
               <ListH3>
                 <PageAnchor href={link.href}>{link.title}</PageAnchor>
               </ListH3>
