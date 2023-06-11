1a51f84c5a2b3e301527842c8caaa505447d3692
inaniwaudon <w.yuto0916@gmail.com>
2023-06-11 23:53:53 +0900

    change the screenname of Twitter

--- src/const/index.ts
+++ src/const/index.ts
@@ -32,8 +32,8 @@ export const photos: Photo[] = [
 export const links: Link[] = [
   {
     platform: 'Twitter',
-    name: '@kyoto_mast21',
-    url: 'https://twitter.com/kyoto_mast21',
+    name: '@kyoto_inaniwa',
+    url: 'https://twitter.com/kyoto_inaniwa',
     color: '#1da1f2',
   },
   {
