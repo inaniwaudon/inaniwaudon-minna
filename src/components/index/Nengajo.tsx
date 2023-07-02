4761f1123deb797de30dd4988e4c2f69254c4c23
inaniwaudon <w.yuto0916@gmail.com>
2023-07-03 04:55:22 +0900

    improve top page link

--- src/components/index/Nengajo.tsx
+++ src/components/index/Nengajo.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import styled from 'styled-components';
-import Checkbox from './common/Checkbox';
+import Checkbox from '../common/Checkbox';
 import { nengajo } from '@/const';
 
 const Header = styled.header`
