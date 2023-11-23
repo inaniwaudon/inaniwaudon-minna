b236d3080f21192b3b3149f05c6ff78eeed55cc4
inaniwaudon <w.yuto0916@gmail.com>
2023-11-24 03:10:43 +0900

    implement reaction api

--- create.sql
+++ create.sql
@@ -8,3 +8,12 @@ CREATE TABLE tanka(
   deleted_at TEXT,
   created_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime'))
 );
+
+CREATE TABLE tanka_reaction(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  tanka_id INTEGER NOT NULL,
+  ip TEXT NOT NULL,
+  reaction TEXT NOT NULL,
+  created_at TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime')),
+  FOREIGN KEY (tanka_id) REFERENCES tanka(id)
+);
